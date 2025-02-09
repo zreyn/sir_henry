67e63f81a896679a29267e693bdfbfa2be851a32
zreyn <zane.reynolds@gmail.com>
2025-02-09 13:41:06 -0500

    try out sphinx

--- agent/src/stt.py
+++ agent/src/stt.py
@@ -1,6 +1,5 @@
 import whisper
 import warnings
-
 import sounddevice as sd
 import numpy as np
 
@@ -20,16 +19,21 @@ def read_from_file(model, filename):
 def read_from_mic(mic, model, sample_rate=44100, duration=3):
     print("Recording...")
     fs = mic["default_samplerate"] # Sample rate
-    myrecording = sd.rec(int(fs * duration), samplerate=fs, channels=1)
+    audio_data = sd.rec(int(duration * fs), dtype='int16', samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
     print("Recording finished.")
 
-    audio_data = (
-        myrecording.flatten().astype(np.float32) / np.finfo(myrecording.dtype).max
-    )  # Normalize
-    audio = whisper.pad_or_trim(audio_data, whisper.audio.SAMPLE_RATE)
+    print("Playing back audio...")
+    # Play back the recorded audio
+    sd.play(audio_data, fs)
+    sd.wait()  # Wait for playback to finish
+    print("Playback finished.")
+
+    print("Padding and spectrogramming...")
+    audio = whisper.pad_or_trim(audio_data.flatten().astype(np.float32))
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
-    print("transcribing")
+    
+    print("Transcribing...")
     result = model.transcribe(mel, language='en')
     print(result)
     return result["text"]
@@ -41,9 +45,7 @@ if __name__ == "__main__":
     warnings.filterwarnings(
         "ignore", message="FP16 is not supported on CPU; using FP32 instead"
     )
-    model = whisper.load_model(
-        "base"
-    )  # Load a model (tiny, base, small, medium, large)
+    model = whisper.load_model("base")  # (tiny, base, small, medium, large)
 
     # print("Reading from file")
     # result = read_from_file(model, "test2.wav")
@@ -54,3 +56,28 @@ if __name__ == "__main__":
     result = read_from_mic(mic, model)
     print(result)
 
+
+
+
+# import whisper
+# import warnings
+# import sounddevice as sd
+# import numpy as np
+
+# def get_default_input_device():
+#     """Prints information about the default input device."""
+#     default_input_device_index = sd.default.device["input"]  # Get the index of the default input device
+#     devices = sd.query_devices()  # Get a list of all devices
+#     return devices[default_input_device_index]  # Get the device info
+
+# model = whisper.load_model("base")  # (tiny, base, small, medium, large)
+# mic = get_default_input_device()
+# fs = mic["default_samplerate"] # Sample rate
+# duration = 3
+
+# audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1) ; sd.wait()
+
+# sd.play(audio_data, fs) ; sd.wait()
+
+# audio = whisper.pad_or_trim(audio_data.flatten().astype(np.float32))
+# mel = whisper.log_mel_spectrogram(audio).to(model.device)
